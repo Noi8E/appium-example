7c3730247875ff1f5510f6f2c3314fe5c94c05ed
Fedor <noi8e@ya.ru>
2022-01-06 12:14:36 +0300

    add other tests + upd owner

--- src/test/java/helpers/ConfigSettings.java
+++ src/test/java/helpers/ConfigSettings.java
@@ -1,6 +1,7 @@
 package helpers;
 
 import config.BrowserstackConfig;
+import config.DevicesConfig;
 import org.aeonbits.owner.ConfigFactory;
 
 import static io.restassured.RestAssured.config;
@@ -8,10 +9,12 @@ import static io.restassured.RestAssured.config;
 public class ConfigSettings {
 
     public static BrowserstackConfig browserstackConfig = ConfigFactory.create(BrowserstackConfig.class,System.getProperties());
+    public static DevicesConfig devicesConfig = ConfigFactory.create(DevicesConfig.class,System.getProperties());
 
     public static void setConfig() {
         String user = browserstackConfig.getUser();
         String key = browserstackConfig.getKey();
+        String device = devicesConfig.getBrowserstack();
     }
 
 
--- src/test/java/tests/local/AndroidLocalSelenideTests.java
+++ src/test/java/tests/local/AndroidLocalSelenideTests.java
@@ -5,9 +5,7 @@ import io.appium.java_client.MobileBy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import tests.TestBase;
 
-import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
@@ -37,6 +35,28 @@ public class AndroidLocalSelenideTests extends LocalTestBase {
 
         step("check preview", () -> {
             $(MobileBy.id("org.wikipedia.alpha:id/link_preview_extract")).shouldBe(visible);
+        });
+    }
+    @Test
+    @DisplayName("Can edit language settings")
+    void editLanguageTest() {
+        step("can Add Language", () -> {
+            $x("//*[@text='ADD OR EDIT LANGUAGES']").click();
+            $x("//*[@text='ADD LANGUAGE']").click();
+            $x("//*[@text='Русский']").click();
+        });
+        step("A language is added", () -> {
+            $x("//*[@text='RU']").shouldBe(visible);
+        });
+        step("Can deleted some language set", () -> {
+            $x("//*[@content-desc='More options']").click();
+            $(MobileBy.id("org.wikipedia.alpha:id/title")).click();
+            $x("//*[@text='RU']").click();
+            $(MobileBy.id("org.wikipedia.alpha:id/menu_delete_selected")).click();
+            $(MobileBy.id("android:id/button1")).click();
+        });
+        step("check succesfully deleted language item", () -> {
+            $x("//*[@text='RU']").shouldNot(visible);
         });
     }
 
